bd12ac6e7a2bec09d4879285994d4c858a4616b1
Peace <peacess@163.com>
2025-04-30 15:25:14 +0800

    mcp sqlite

---
+++ demo/mcp_sqlite/server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	mcp_golang "github.com/metoro-io/mcp-golang"
+	"github.com/metoro-io/mcp-golang/transport/stdio"
+	mcpsqlite "github.com/peacess/go/demo/mcp_sqlite"
+)
+
+func main() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	server := mcp_golang.NewServer(stdio.NewStdioServerTransport())
+	serive := mcpsqlite.ServiceSqlite{}
+
+	serive.Register(server)
+
+	// Start the server
+	log.Println("MCP Server is now running and waiting for requests...")
+	err := server.Serve()
+	if err != nil {
+		println(err)
+	} else {
+		println("server started")
+		<-interrupt
+	}
+	println("server exit")
+}
---
+++ demo/mcp_sqlite/service_sqlite.go
@@ -0,0 +1,10 @@
+package mcpsqlite
+
+import mcp_golang "github.com/metoro-io/mcp-golang"
+
+type ServiceSqlite struct {
+}
+
+func (c *ServiceSqlite) Register(server *mcp_golang.Server) {
+
+}
