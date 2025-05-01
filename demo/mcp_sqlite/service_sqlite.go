f917e6741eba9f4d5d3df5a639a4e09879a01981
Peace <peacess@163.com>
2025-05-01 08:28:32 +0800

    service sqlite

--- demo/mcp_sqlite/service_sqlite.go
+++ demo/mcp_sqlite/service_sqlite.go
@@ -6,5 +6,37 @@ type ServiceSqlite struct {
 }
 
 func (c *ServiceSqlite) Register(server *mcp_golang.Server) {
-
+	{
+		type ExecuteSqlArguments struct {
+			Sql string `json:"Sql" jsonschema:"required,description=SQL to execute"`
+		}
+		server.RegisterTool("execute_sql", "Execute a SQL on the SQLite database", func(args ExecuteSqlArguments) (res *mcp_golang.ToolResponse, err error) {
+			res = &mcp_golang.ToolResponse{}
+			err = nil
+			println(args.Sql)
+			return
+		})
+	}
+	{
+		type ListTablesArguments struct {
+			Sql string `json:"Sql" jsonschema:"required,description=SQL to execute"`
+		}
+		server.RegisterTool("list_tables", "list all tables in the SQLite database", func(args ListTablesArguments) (res *mcp_golang.ToolResponse, err error) {
+			res = &mcp_golang.ToolResponse{}
+			err = nil
+			println(args.Sql)
+			return
+		})
+	}
+	// {
+	// 	type ListTablesArguments struct {
+	// 		Sql string `json:"Sql" jsonschema:"required,description=SQL to execute"`
+	// 	}
+	// 	server.RegisterTool("create_table", "create table in the SQLite database", func(args ListTablesArguments) (res *mcp_golang.ToolResponse, err error) {
+	// 		res = &mcp_golang.ToolResponse{}
+	// 		err = nil
+	// 		println(args.Sql)
+	// 		return
+	// 	})
+	// }
 }
